ededdfbe3739c1179df136926d63a513196aedcf
Mulayam .S <o_Mulayam.S@fusionsol.com>
2020-02-06 11:45:50 +0530

    Adding few classes and folders

---
+++ BilBest/BilBest/CustomControls/CustomEntry.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace BilBest.CustomControls
+{
+    public class CustomEntry : Entry
+    {
+    }
+}
