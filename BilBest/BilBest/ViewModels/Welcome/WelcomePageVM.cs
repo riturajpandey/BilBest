ee53934fd0628865dd57756ad8288571f336148f
Mulaym SIngh Yadav <rituraj.pandey786@gmail.com>
2020-02-07 13:20:01 +0530

    Worked On Welcome Screen

---
+++ BilBest/BilBest/ViewModels/Welcome/WelcomePageVM.cs
@@ -0,0 +1,42 @@
+﻿using BilBest.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace BilBest.ViewModels.Welcome
+{
+    public class WelcomePageVM : BaseViewModel
+    {
+        #region Constructor
+        public WelcomePageVM(INavigation nav)
+        {
+            Navigation = nav;
+            WelcomeList = new ObservableCollection<WelcomePageModel>
+            {
+                new WelcomePageModel{Image="medicine.png",SubTitle="Cebinizde ki",Title="Doktorunuz",Description="Bu alana tanıtım kartları, sloganları ve yazılar eklenebilir. Kayar slider şeklinde ve sayısı belirlenebilir. Ama maksimum 5 tane yeterlidir."},
+                new WelcomePageModel{Image="medicine.png",SubTitle="Cebinizde ki",Title="Doktorunuz",Description="Bu alana tanıtım kartları, sloganları ve yazılar eklenebilir. Kayar slider şeklinde ve sayısı belirlenebilir. Ama maksimum 5 tane yeterlidir."},
+                new WelcomePageModel{Image="medicine.png",SubTitle="Cebinizde ki",Title="Doktorunuz",Description="Bu alana tanıtım kartları, sloganları ve yazılar eklenebilir. Kayar slider şeklinde ve sayısı belirlenebilir. Ama maksimum 5 tane yeterlidir."},
+                new WelcomePageModel{Image="medicine.png",SubTitle="Cebinizde ki",Title="Doktorunuz",Description="Bu alana tanıtım kartları, sloganları ve yazılar eklenebilir. Kayar slider şeklinde ve sayısı belirlenebilir. Ama maksimum 5 tane yeterlidir."},
+            };
+        }
+        #endregion
+
+        #region Properties
+        private ObservableCollection<WelcomePageModel> _WelcomeList;
+        public ObservableCollection<WelcomePageModel> WelcomeList
+        {
+            get { return _WelcomeList; }
+            set
+            {
+                if (_WelcomeList != value)
+                {
+                    _WelcomeList = value;
+                    OnPropertyChanged("WelcomeList");
+                }
+            }
+        }
+        #endregion
+    }
+}
--- BilBest/BilBest/Views/Welcome/WelcomePage.xaml.cs
+++ BilBest/BilBest/Views/Welcome/WelcomePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BilBest.ViewModels.Welcome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,17 @@ namespace BilBest.Views.Welcome
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class WelcomePage : ContentPage
     {
+        //Define Properties Here
+        protected WelcomePageVM WelcomePageVM;
+
+        #region Constructor
         public WelcomePage()
         {
             InitializeComponent();
-        }
+            WelcomePageVM = new WelcomePageVM(this.Navigation);
+            BindingContext = WelcomePageVM;
+            cardlistview.ItemsSource = WelcomePageVM.WelcomeList;
+        } 
+        #endregion
     }
 }
